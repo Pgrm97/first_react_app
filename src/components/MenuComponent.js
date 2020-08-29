6c258485da2b505ca8a0c9018b34a59f9b3fd864
Pedro Guillermo Rodríguez <20155001@ce.pucmm.edu.do>
2020-08-29 02:10:27 -0400

    Added all menu items

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -52,7 +52,15 @@ class Menu extends Component {
             //mt-5 means give top margin of 5 units.
             return (
                 <div key={dish.id} className="col-12 mt-5">
-                    Hi
+                    <Media tag="li">
+                        <Media left middle>
+                            <Media object src={dish.image} alt={dish.name}/>
+                        </Media>
+                        <Media body className="ml-5">
+                            <Media heading>{dish.name}</Media>
+                            <p>{dish.description}</p>
+                        </Media>
+                    </Media>
                 </div>
             )
         });
