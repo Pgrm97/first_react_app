76b71c127e328ea30d1155290c7151e3adf32035
Pedro Guillermo Rodríguez <20155001@ce.pucmm.edu.do>
2020-08-29 01:29:27 -0400

    Added dependencies

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is me trying out this new course to get a job in Germany!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://bbc.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Read the latest news
         </a>
       </header>
     </div>
